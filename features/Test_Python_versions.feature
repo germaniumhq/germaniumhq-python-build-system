640214f0bac682e72db045f9cd9d07e028f50cd1
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2018-03-22 07:13:00 +0100

    Ensure pip is installed.

--- features/Test_Python_versions.feature
+++ features/Test_Python_versions.feature
@@ -5,18 +5,26 @@ Scenario: Test python 2.7
 Given I run the docker container for 'germaniumhq/python:2.7'
 When I get the version of the default python command
 Then it is version '2.7'
+When I check if pip it's installed
+Then it's instaled in the container
 
 Scenario: Test python 3.4
 Given I run the docker container for 'germaniumhq/python:3.4'
 When I get the version of the default python command
 Then it is version '3.4'
+When I check if pip it's installed
+Then it's instaled in the container
 
 Scenario: Test python 3.5
 Given I run the docker container for 'germaniumhq/python:3.5'
 When I get the version of the default python command
 Then it is version '3.5'
+When I check if pip it's installed
+Then it's instaled in the container
 
 Scenario: Test python 3.6
 Given I run the docker container for 'germaniumhq/python:3.6'
 When I get the version of the default python command
 Then it is version '3.6'
+When I check if pip it's installed
+Then it's instaled in the container
--- features/steps/test_python_versions.py
+++ features/steps/test_python_versions.py
@@ -11,19 +11,36 @@ use_step_matcher("re")
 
 @step("I run the docker container for '(.*)'")
 def run_docker_container(context, container_name: str) -> None:
-    output = subprocess.check_output(['docker', 'run', '-t', '--rm', container_name, 'python', '--version']) # type: bytes
+    context.container_name = container_name
+
+
+@step("I check if pip it's installed")
+def run_pip_to_see_if_it_is_installed(context):
+    output = subprocess.check_output(['docker', 'run', '-t', '--rm', 
+                        context.container_name, 
+                        'pip', '--version']) # type: bytes
     context.docker_output = output.decode('utf-8')
-    
+
+
+@step("it's instaled in the container")
+def check_pip_version_name(context):
+    print(context.docker_output)
+
 
 @step("I get the version of the default python command")
 def get_python_version(context) -> None:
+    output = subprocess.check_output(['docker', 'run', '-t', '--rm', 
+                        context.container_name, 
+                        'python', '--version']) # type: bytes
+    context.docker_output = output.decode('utf-8')
+
+
+@step("it is version '(.*)'")
+def check_python_version(context, container_name: str) -> None:
     m = VERSION_MATCHER.match(context.docker_output)
     if not m:
         raise Exception("Unable to parse version from: %s" % context.docker_output)
 
     context.python_version = m.group(2)
 
-
-@step("it is version '(.*)'")
-def check_python_version(context, container_name: str) -> None:
     assertEqual(container_name, context.python_version)
